0d5f13c66a08365a88a0b12532a6bc0026470401
Tiffany M <67019104+tiffany-m@users.noreply.github.co>
2023-02-01 16:24:35 -0500

    test

--- scripts.js
+++ scripts.js
@@ -1,10 +1,5 @@
 const gridContainer = document.getElementById('grid-container');
 const grid = document.getElementById('grid');
-const increaseBtn = document.getElementById('increase-btn');
-const decreaseBtn = document.getElementById('decrease-btn');
-const colorPicked = document.getElementById('color-picker');
-const clearBtn = document.getElementById('clear-btn');
-const gridSizeText = document.getElementById('grid-size-text');
 let color = '#00C5CD';
 let size = 24;
 
@@ -30,68 +25,3 @@ function createGrid(numRowsCols) {
 
 // create default grid and add proper eventListeners to squares
 createGrid(size);
-addColorToSquares();
-clearGrid();
-
-function updateSizeOnScreen() {
-    gridSizeText.innerText = size;
-}
-
-function gridRemove() {
-    const rows = document.querySelectorAll('.row');
-    const cols = document.querySelectorAll('.column');
-
-    cols.forEach(col => col.remove());
-    rows.forEach(row => row.remove());
-}
-
-function createNewGrid(numRowsCols) {
-    gridRemove(); 
-    createGrid(numRowsCols); 
-    gridContainer.appendChild(grid);
-    addColorToSquares();
-    clearGrid();
-}
-
-function addColorToSquares() {
-    const squares = document.querySelectorAll('.column');
-
-    squares.forEach(square => {
-        square.addEventListener('mouseover', () => {
-            square.style.backgroundColor = color;
-        })
-    })
-}
-
-function clearGrid() {
-    const squares = document.querySelectorAll('.column');
-
-    clearBtn.addEventListener('click', () => {
-        squares.forEach(square => {
-            square.style.backgroundColor = 'white';
-        })
-    })
-}
-
-increaseBtn.addEventListener('click', ()=> {
-    size++ 
-
-    if(size > 64) size = 64;
-    
-    updateSizeOnScreen();
-    createNewGrid(size);
-})
-
-decreaseBtn.addEventListener('click', () => {
-    size--
-
-    if (size < 8) size = 8;
-
-    updateSizeOnScreen();
-    createNewGrid(size, size);
-})
-
-colorPicked.addEventListener('change', (e) => {
-    (color = e.target.value)
-    console.log(e);
-});
